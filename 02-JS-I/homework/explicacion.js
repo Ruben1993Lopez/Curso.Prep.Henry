2ef5a6453349ae5b0aa085c4ab5c5679a70d0172
ruben <ruben20893@gmail.com>
2022-02-11 11:29:55 -0300

    trabajo

---
+++ 02-JS-I/homework/explicacion.js
@@ -0,0 +1,6 @@
+variables : es una forma de guardar algo, para usar mas tarde. ej: var nombre= Ruben.
+strings : son bloques de textos que a diferencia de variables, se coloca entre comillas 
+funciones: es un procedimiento o intrucciones que realiza una tarea o calcula un valor
+return : es una funcion que le damos para que nos devuelva un dato o calculo.
+if : es una funcion que le podemos poner una condicion para que nos devuelva si algo es verdadero o falso
+booleanos : es una variable que tiene dos opciones, true o false.
